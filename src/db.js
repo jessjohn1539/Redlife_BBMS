28434e0893f0595316ff9179e92fec3e3e22d9e9
Jess John <jessjohn1539@gmail.com>
2023-04-15 01:38:03 +0530

    modified:   src/db.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '@Ys2drrizfs',
-});
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+  });
 
 const createDatabase = () => {
     connection.query(`CREATE DATABASE IF NOT EXISTS BBMSRedlife`, (err, result) => {
