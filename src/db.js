81caad81736195c2003b7b0a43228fa4a8659ff5
Jess John <jessjohn1539@gmail.com>
2023-04-23 19:15:59 +0530

    modified:   src/db.js

--- src/db.js
+++ src/db.js
@@ -4,7 +4,8 @@ const connection = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 connection.getConnection((err, connection) => {
